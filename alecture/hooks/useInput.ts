2d9cbbab00a5964688b341a986c82b7ac87017ea
lovaoi777 <lovaoi777@naver.com>
2023-11-08 19:53:02 +0900

    ✨HooKs(UseInput)

---
+++ alecture/hooks/useInput.ts
@@ -0,0 +1,11 @@
+import { useState, useCallback, Dispatch, SetStateAction } from 'react';
+
+const useInput = <T = any>(initialData: T): [T, (e: any) => void, Dispatch<SetStateAction<T>>] => {
+  const [value, setValue] = useState(initialData);
+  const handler = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+  return [value, handler, setValue];
+};
+
+export default useInput;
