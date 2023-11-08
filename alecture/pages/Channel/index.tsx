04cb03f4e4f67c12004750854a081e8abad77520
lovaoi777 <lovaoi777@naver.com>
2023-11-08 19:56:21 +0900

    ✨ Channel Component

---
+++ alecture/pages/Channel/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Header } from './styles';
+import Workspace from '@layouts/Workspace/Workspace';
+const Channel = () => {
+  return (
+    <Workspace>
+      <Container>
+        <Header>채널!</Header>
+      </Container>
+    </Workspace>
+  );
+};
+
+export default Channel;
